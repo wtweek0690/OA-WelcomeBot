59526fb4c61e9c5c4509c62981fd7aa6d91d01db
github <github@hamidout.xyz>
2020-09-10 15:39:25 +0000

    fixes

--- server.js
+++ server.js
@@ -7,16 +7,18 @@ client.on('ready', () => {
 });
 
 client.on('guildMemberAdd', member => {
-    member.send("https://i.imgur.com/adYZUJs.jpg");
+    member.send("http://46.101.247.248/1.png");
+    member.send("http://46.101.247.248/02.gif");
+    member.send("http://46.101.247.248/3.png");
     member.send("Welcome To Our OA Community! We'd like to show you around!")
-    member.send(`> Agree to see tutorial, disagree to skip! ${config.first_emote}`).then(msg => {
+    member.send(`> To see the tutorial react with ✅ To skip simply react with ❌ ${config.first_emote}`).then(msg => {
         msg.react(config.reaction_yes)
         msg.react(config.reaction_no)
         msg.awaitReactions((reaction, user) => user.id == member.user.id && (reaction.emoji.name == config.reaction_yes || reaction.emoji.name == config.reaction_no),
         { max: 1, time: 6.048e+8 }).then(collected => {
                 if (collected.first().emoji.name == config.reaction_yes) {
                     
-                    member.send("https://i.imgur.com/DzUxHJG.jpg").then(msg => {
+                    member.send("http://46.101.247.248/4.png").then(msg => {
                         member.send([
                             "*This is your home page, you will access everything you need to learn about our community here! I'll let you get settled, once you're ready to explore more of OA, click YES!*",
                             "",
@@ -28,7 +30,7 @@ client.on('guildMemberAdd', member => {
                             { max: 1, time: 6.048e+8 }).then(collected1 => {
                                     if (collected1.first().emoji.name == config.reaction_yes) {
                                         
-                                        member.send("https://i.imgur.com/fSO0MU8.jpg").then(msg2 => {
+                                        member.send("http://46.101.247.248/5.png").then(msg2 => {
                                             member.send([
                                                 "*All the information you'll ever need will be here. Travel and explore these channels, once you're ready to explore more of our OA, then click YES*",
                                                 "",
@@ -41,7 +43,7 @@ client.on('guildMemberAdd', member => {
                                                 { max: 1, time: 6.048e+8 }).then(collected2 => {
                                                         if (collected2.first().emoji.name == config.reaction_yes) {
                                                             
-                                                            member.send("https://i.imgur.com/ojhHiBa.jpg").then(msg4 => {
+                                                            member.send("http://46.101.247.248/6.png").then(msg4 => {
                                                                 member.send([
                                                                     `*To stand out within our OA, make sure to select the roles that apply you in the <#${config.roles_channelid}> channel! You may do this now, once you're ready to explore more of our OA, then click YES*`,
                                                                     "",
@@ -53,12 +55,12 @@ client.on('guildMemberAdd', member => {
                                                                     { max: 1, time: 6.048e+8 }).then(collected3 => {
                                                                             if (collected3.first().emoji.name == config.reaction_yes) {
                                                                                 
-                                                                                member.send("https://i.imgur.com/gJ4gcTV.jpg").then(msg6 => {
+                                                                                member.send("http://46.101.247.248/7.png").then(msg6 => {
                                                                                     member.send([
-                                                                                        `*You've made it <@${member.user.id}> through the tutorial & we're so proud of you! Remember to invite all your friends and spread the news about our wonderful adventures!!! React for your special role!*`,
+                                                                                        `*You've made it <@${member.user.id}> through the tutorial & we're so proud of you! Remember to invite all your friends and spread the news about our wonderful adventures!*`,
                                                                                     ]).then(msg7 => {
                                                                                         msg7.react(config.special_react_emote)
-                                                                                        member.send("https://i.imgur.com/UD1tj91.jpg").then(msg8 => {
+                                                                                        member.send("http://46.101.247.248/8.png").then(msg8 => {
                                                                                             member.send([
                                                                                                 `*Start Chatting Now <#${config.mainchat_channelid}>*`,
                                                                                             ])
